8570071441674fceece324f0ff3e0aeeaec82ff3
Javier Clavijo <jjclavijo@gmail.com>
2021-06-11 20:58:01 -0300

    r2 + arreglado si no empieza en silencio.

---
+++ purgelist.py
@@ -0,0 +1,28 @@
+with open('list.files') as f:
+    lineas = f.readlines
+dic = {}
+for i in f:
+    v,k = f.strip().split()
+    dic[k]=v
+for i in lines:
+    v,k = i.strip().split()
+    dic[k]=v
+with open('list.files') as f:
+    lineas = f.readlines
+for i in lineas:
+    v,k = i.strip().split()
+    dic[k]=v
+dic
+with open('list.files') as f:
+    lineas = f.readlines()
+for i in lineas:
+    v,k = i.strip().split()
+    dic[k]=v
+dic
+with open('prelist.txt','w') as f:
+    for k,x in dic.items()
+with open('prelist.txt','w') as f:
+    for k,x in dic.items():
+        if v == 'file':
+            print(f'file {k}',file=f)
+hist -f purgelist.py
